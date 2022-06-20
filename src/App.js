480c7c5557e3e757ba3685737cd716843dd3f6b1
motishokri <motahare.shokri@yahoo.com>
2022-06-20 18:50:04 +0430

    Update App

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ export default function App() {
           <Dictionary />
         </main>
         <footer className="App-footer">
-          <small>Coded by SheCodes</small>
+          <small>Coded by Motahare Shokri</small>
         </footer>
       </div>
     </div>
