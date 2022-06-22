5e5a3e34be5ca2d3fbcc7758eadcd6b11f56d47a
motishokri <motahare.shokri@yahoo.com>
2022-06-22 21:11:29 +0430

    Update Phonetic

--- src/Results.js
+++ src/Results.js
@@ -1,15 +1,16 @@
 import React from "react";
-import Meaning from "./Meaning";
 import Phonetic from "./Phonetic";
+import Meaning from "./Meaning";
+
 export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
-        {props.results.phontic.map(function (phontic, index) {
+        {props.results.phonetics.map(function (phonetic, index) {
           return (
             <div key={index}>
-              <Phonetic phontic={phontic} />
+              <Phonetic phonetic={phonetic} />
             </div>
           );
         })}
