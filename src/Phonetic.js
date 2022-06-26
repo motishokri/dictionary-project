1dca84eb953131bd6694f141b0681efe2e35de91
motishokri <motahare.shokri@yahoo.com>
2022-06-26 21:58:42 +0430

    Update Phonetic

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,11 +1,18 @@
 import React from "react";
-import ReactAudioPlayer from "react-audio-player";
+import "./Phonetic.css";
 
 export default function Phonetic(props) {
   console.log(props.phonetic);
   return (
     <div className="Phonetic">
-      <ReactAudioPlayer src="my_audio_file.ogg" autoPlay controls />
+      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+        <input
+          class="fluid"
+          type="image"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Speaker_Icon_gray.svg/800px-Speaker_Icon_gray.svg.png"
+          alt="Listen"
+        />
+      </a>
       <br />
       {props.phonetic.text}
     </div>
