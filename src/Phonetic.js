6119784f610f8bc7e0d186641e8d9d060a58f09b
motishokri <motahare.shokri@yahoo.com>
2022-06-27 19:04:58 +0430

    Update Phonetic

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -6,12 +6,7 @@ export default function Phonetic(props) {
   return (
     <div className="Phonetic">
       <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
-        <input
-          class="fluid"
-          type="image"
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Speaker_Icon_gray.svg/800px-Speaker_Icon_gray.svg.png"
-          alt="Listen"
-        />
+        Listen
       </a>
       <span className="text">{props.phonetic.text}</span>
     </div>
